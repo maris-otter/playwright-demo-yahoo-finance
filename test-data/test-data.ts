3a0b5bfab7b8b6851011c493e51828719ebabfa3
Mike <61036167+maris-otter@users.noreply.github.com>
2025-08-01 01:09:43 -0400

    test data and home page smoke test

---
+++ test-data/test-data.ts
@@ -0,0 +1,11 @@
+// Store data for test runs
+
+export const STOCKS = {
+    DEFAULT_TICKER : 'S&P',
+
+    REDDIT: {
+    searchTerm: 'Reddit',
+    optionBarValue: 'RDDT Reddit, Inc. Equity NYQ',
+    expectedResult: 'Reddit, Inc. (RDDT)'
+  }
+}
--- tests/stock-search.spec.ts
+++ tests/stock-search.spec.ts
@@ -1,11 +1,20 @@
 import { test, expect } from '@playwright/test';
+import { STOCKS } from '../test-data/test-data';
 
-test('test', async ({ page }) => {
-  await page.goto('https://ca.finance.yahoo.com/');
+test('Stock Search', async ({ page }) => {
+  const stock = STOCKS.REDDIT;
+  await page.goto('/', {waitUntil: "commit"});
   //await expect(page.getByRole('link', { name: 'Yahoo Finance', exact: true })).toBeVisible();
   await expect(page.locator('#ybar-logo')).toBeVisible();
-  await page.getByRole('textbox', { name: 'Search query' }).fill('Reddit');
-  await page.getByRole('option', { name: 'RDDT Reddit, Inc. Equity NYQ' }).click();
-  await expect(page.getByTestId('quote-hdr').locator('h1')).toContainText('Reddit, Inc. (RDDT)');
+  await page.getByRole('textbox', { name: 'Search query' }).fill(stock.searchTerm); // Pulled from test-data file
+  await page.getByRole('option', { name: stock.optionBarValue }).click();
+  await expect(page.getByTestId('quote-hdr').locator('h1')).toContainText(stock.expectedResult);
 
+});
+
+test('Home Page Smoke', async ({ page }) => {
+  await page.goto('/', {waitUntil: "commit"});
+  await expect(page.getByTestId('hero-lead-story').getByRole('list')).toBeVisible();
+  //await expect(page.getByRole('link', { name: new RegExp(DEFAULT_TICKER) }).getByTestId('sparkline')).toBeVisible();
+  await expect(page.getByRole('link', { name: /S&P/ }).getByTestId('sparkline')).toBeVisible();
 });
